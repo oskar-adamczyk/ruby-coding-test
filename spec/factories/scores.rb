4354805bcab76ae4d8987ae8b34b9f6f018be887
oskar-adamczyk <oskar.adamczyk@ptd-interactive.co>
2022-03-09 22:28:38 +0100

    Prepare RED tests for removing score

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :score do
     entry { build :leaderboard_entry }
     value { Faker::Number.number digits: 2 }
+
+    trait :without_entry do
+      entry { nil }
+    end
   end
 end
