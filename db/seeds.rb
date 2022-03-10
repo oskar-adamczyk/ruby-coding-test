4068ef6e827d536538b42de25b8736598b5db1e7
oskar-adamczyk <oskar.adamczyk@ptd-interactive.co>
2022-03-10 16:28:54 +0100

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,19 @@
 require "database_cleaner"
 require "faker"
 
+def seed_leadeboard
+  return Rails.logger.info "Leaderboard exists, skipping" if Leaderboard.find_by(name: "My Leaderboard")
+
+  FactoryBot.create(
+    :leaderboard, name: "My Leaderboard", entries: [
+      FactoryBot.build(:leaderboard_entry, :with_scores, username: "Jane", score: 3),
+      FactoryBot.build(:leaderboard_entry, :with_scores, username: "Jack", score: 10),
+      FactoryBot.build(:leaderboard_entry, :with_scores, username: "John", score: 9),
+      FactoryBot.build(:leaderboard_entry, username: "June")
+    ]
+  )
+end
+
 if ENV["CLEAR_DB"]
   raise "Do not clear DB on production environment" if Rails.env.production?
 
@@ -10,11 +23,4 @@ if ENV["CLEAR_DB"]
   DatabaseCleaner.clean
 end
 
-FactoryBot.create(
-  :leaderboard, name: "My Leaderboard", entries: [
-    FactoryBot.build(:leaderboard_entry, :with_scores, username: "Jane", score: 3),
-    FactoryBot.build(:leaderboard_entry, :with_scores, username: "Jack", score: 10),
-    FactoryBot.build(:leaderboard_entry, :with_scores, username: "John", score: 9),
-    FactoryBot.build(:leaderboard_entry, username: "June")
-  ]
-)
+seed_leadeboard
